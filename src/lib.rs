48af4a52617f3de4a660e3788e7a32c8dd5352cd
Jamrok <3874968+jamrok@users.noreply.github.com>
2023-07-24 21:58:10 -0500

    Project 2: Setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![deny(clippy::all)]
 // #![deny(missing_docs)]
 
-use std::collections::HashMap;
+pub use std::io::Result;
+use std::{collections::HashMap, path::PathBuf};
 
 #[derive(Default)]
 pub struct KvStore {
@@ -13,15 +14,55 @@ impl KvStore {
         Self::default()
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        self.data.get(&key).cloned()
+    /// Returns the string value of the given string key.
+    ///
+    /// If the key does not exist, return None.
+    ///
+    /// `kvs` first searches the in-memory index and retrieves the corresponding log-pointer, which
+    /// is then used to find the `get` command in the log (on-disk). The command is then evaluated
+    /// and the result is returned.
+    ///
+    /// # Errors
+    ///
+    /// If the value is not read successfully.
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        Ok(self.data.get(&key).cloned())
     }
 
-    pub fn remove(&mut self, key: String) {
+    /// Removes the given string key.
+    ///
+    /// `kvs` first writes the `rm` command to the sequential log on-disk and then removes the key
+    /// from the in-memory index.
+    ///
+    /// # Errors
+    ///
+    /// - If the key does not exist.
+    /// - If the key is not removed successfully.
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.data.remove(&key);
+        Ok(())
     }
 
-    pub fn set(&mut self, key: String, value: String) {
+    /// Saves the given string value to the given string key.
+    ///
+    /// `kvs` first writes the `set` command to disk in a sequential log, then stores the log
+    /// pointer (file offset) of that command to the in-memory index (i.e. the value stored
+    /// in-memory is the log pointer of the command).
+    ///
+    /// # Errors
+    ///
+    /// If the value is not written successfully.
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.data.insert(key, value);
+        Ok(())
+    }
+
+    /// Open the KvStore at a given path and return the KvStore.
+    ///
+    /// # Errors
+    ///
+    /// If there was a problem opening the KvStore.
+    pub fn open(_path: impl Into<PathBuf>) -> Result<KvStore> {
+        Ok(KvStore::new())
     }
 }
